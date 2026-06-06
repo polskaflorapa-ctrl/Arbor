7fb055346b621c94ac7dcea2ce6a83f9cd624b43
polskaflorapa-ctrl <polskaflorapa@gmail.com>
2026-06-06 17:13:36 +0200

    Fix Expo doctor checks

--- mobile/components/haptic-tab.tsx
+++ mobile/components/haptic-tab.tsx
@@ -1,10 +1,12 @@
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
-import { PlatformPressable } from '@react-navigation/elements';
 import * as Haptics from 'expo-haptics';
+import type { ComponentProps } from 'react';
+import { Pressable } from 'react-native';
 
-export function HapticTab(props: BottomTabBarButtonProps) {
+type HapticTabProps = ComponentProps<typeof Pressable>;
+
+export function HapticTab(props: HapticTabProps) {
   return (
-    <PlatformPressable
+    <Pressable
       {...props}
       onPressIn={(ev) => {
         if (process.env.EXPO_OS === 'ios') {
